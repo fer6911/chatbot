248fac77258edc9d31446d672b0dda07083b9fb7
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 10:16:32 -0500

    [back] prueba5

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,17 +1,18 @@
 const verificar = (req, res) => {
 
   try {
-    const VERIFY_TOKEN = "TOKENMETAAPI"; 
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-    console.log("Datos recibidos:", { token, challenge });
+    const tokenMeta = "TOKENMETAAPI";
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
 
-    if (challenge && token && token === VERIFY_TOKEN) {
-      console.log("-Webhook verificado correctamente---");
-      return res.status(200).send(challenge);
+   console.log("Datos recibidos:", { token, challenge }); 
+
+    if (challenge && token && token === tokenMeta) {
+        console.log("Webhook verificado correctamente");
+        return res.status(200).send(challenge);
     }
-
-    console.error("Fallo en verificación:", { tokenRecibido: token, tokenEsperado: VERIFY_TOKEN });
+    
+    console.error("Fallo en verificación:", { tokenRecibido: token, tokenEsperado: tokenMeta });
     return res.status(400).send("Token inválido o falta challenge");
   } catch (e) {
     res.status(400).send();
