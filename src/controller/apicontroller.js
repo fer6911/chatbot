aa5ac5f22cb4d97dae082fa0746e62348ce14958
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 10:23:34 -0500

    [back] token andercode

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,18 +1,18 @@
 const verificar = (req, res) => {
 
   try {
-    const tokenMeta = "TOKENMETAAPI";
+    const tokeandercode = "TOKENMETAAPI";
     const token = req.query["hub.verify_token"];
     const challenge = req.query["hub.challenge"];
 
    console.log("Datos recibidos:", { token, challenge }); 
 
-    if (challenge && token && token === tokenMeta) {
+    if (challenge && token && token === tokeandercode) {
         console.log("Webhook verificado correctamente");
         return res.status(200).send(challenge);
     }
     
-    console.error("Fallo en verificación:", { tokenRecibido: token, tokenEsperado: tokenMeta });
+    console.error("Fallo en verificación:", { tokenRecibido: token, tokenEsperado: tokeandercode });
     return res.status(400).send("Token inválido o falta challenge");
   } catch (e) {
     res.status(400).send();
