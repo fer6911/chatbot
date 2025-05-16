72cf5d93820b4bcad2b73c027f22866e91dcb8e1
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 05:24:14 -0500

    [back] logs para haces debug

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,9 +1,12 @@
 const verificar = (req, res) => {
   res.send("Verificado");
+  console.log("Verificado");
+  
 };
 
 const recibir = (req, res) => {
   res.send("Recibido");
+  console.log("Recibido");
 };
 
 module.exports = {
