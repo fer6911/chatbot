69c38862a693d03947b38797ef0cc41ec2fd9a23
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-15 20:37:05 -0500

    [back]cambio de puerto

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const apiruta = require("./routes/ruta")
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use("/api",apiruta)
