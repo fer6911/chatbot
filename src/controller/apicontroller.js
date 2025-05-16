05ca1396b7c1a78d02b44a05955755d307f46068
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 05:40:45 -0500

    [back] validar token

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -1,13 +1,21 @@
 const verificar = (req, res) => {
 
   try {
-    const tokenmeta = "TOKENAPIMETA";
-    conts token =  req.query[""]
+    const tokenmeta = "TOKENAPIMETA"
+    const token =  req.query["hub.verify_token"]
+    let challenge = req.query["hub.challenge"]
+
+    if(challenge !== null && token !== null && token !== tokenmeta){
+      res.send(challenge)
+    }else {
+      res.status.send(400).send()
+    }
+  console.log(req);
+
   } catch (error) {
     res.status.send(400).send()
   }
-  res.send("Verificado");
-  console.log("Verificado");
+
   
 };
 
