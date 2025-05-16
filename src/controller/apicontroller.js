bf82b2bbeb56342b6f0e05f398826e43fb8306e2
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 10:46:15 -0500

    [back]  validation

--- src/controller/apicontroller.js
+++ src/controller/apicontroller.js
@@ -2,18 +2,17 @@ const verificar = (req, res) => {
 
   try {
     const tokenandercode = "ANDERCODENODEJSAPIMETA";
-    const token = req.query["hub.verify_token"];
-    const challenge = req.query["hub.challenge"];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
 
-   console.log("Datos recibidos:", { token, challenge }); 
+    console.log("Datos recibidos:", { token, challenge });
 
-    if (challenge != null && token != null && token == tokenandercode){
+    if (challenge != null && token != null && token == tokenandercode) {
       console.error("verificación:", { tokenRecibido: token, tokenEsperado: tokenandercode });
-            res.send(challenge);
-        }else{
-          return res.status(400).send("Token inválido o falta challenge");
-            res.status(400).send();
-        }
+      res.send(challenge);
+    } else {
+      return res.status(400).send("Token inválido o falta challenge");
+    }
   } catch (e) {
     res.status(400).send();
   }
