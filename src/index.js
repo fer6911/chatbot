ede06965e439b92f7569cfb7fbbcb087d8e84098
Pablo Fernandez <desarrollofe2@sio.com.co>
2025-05-16 10:29:35 -0500

    [back] url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const router = require('./routes/ruta');
 
 app.use(express.json());
-app.use('/api/', router);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 8080
 
